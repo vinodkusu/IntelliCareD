01a8327a023e375da270a49f4863166d2f98af37
Andro <androdev6717@gmail.com>
2020-11-23 21:41:14 +0530

    conflict fix

--- app/src/main/java/com/themahi/intellicare/Utils.java
+++ app/src/main/java/com/themahi/intellicare/Utils.java
@@ -18,13 +18,7 @@ public static void toastLong(String msg, Context c) {
     }
 
 public static void toastSample(String msg, Context c) {
-<<<<<<< HEAD
-        if (!TextUtils.isEmpty(msg) && c != null) {
-            Toast.makeText(c, "sample", Toast.LENGTH_SHORT).show();
-        }
-=======
-            Toast.makeText(c, "sample", Toast.LENGTH_SHORT).show();
->>>>>>> sample
+Toast.makeText(c, "sample", Toast.LENGTH_SHORT).show();
     }
 
 }
