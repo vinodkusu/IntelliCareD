df377825203147d7ef8237e8448b6424653cc6f3
Andro <androdev6717@gmail.com>
2020-11-23 19:10:47 +0530

    toastLong added in Utils

--- app/src/main/java/com/themahi/intellicare/Utils.java
+++ app/src/main/java/com/themahi/intellicare/Utils.java
@@ -10,4 +10,10 @@ public class Utils {
             Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
         }
     }
+
+public static void toastLong(String msg, Context c) {
+        if (!TextUtils.isEmpty(msg) && c != null) {
+            Toast.makeText(c, msg, Toast.LENGTH_LENGTH).show();
+        }
+    }
 }
